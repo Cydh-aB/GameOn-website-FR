8903d7b42c6f2ecc1a7c733289debc5b6df906de
Anthony <beral.anthony59@gmail.com>
2021-11-25 14:13:03 +0100

    Ajout des vréfications prénom / nom / mail / date de naissance en dynamique

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -26,4 +26,58 @@ closeModal[0].addEventListener("click", function() {
   modalbg.style.display = "none";
 });
 
+document.forms["reserve"].addEventListener("submit", function(e) {
+	 
+  var erreur;
+  var erreurVille;
+  var erreurPrenom;
+  var erreurNom;
+  var erreurMail;
+  
+  var inputs = this;
 
+  if (inputs["first"].value == "" || inputs["first"].length < 2) {
+    erreurPrenom = "Veuillez entrer 2 caractères ou plus pour le champ du Prénom.";
+    document.getElementById("first_erreur").innerHTML = erreurPrenom;
+  }
+
+  if (inputs["last"].value == "" || inputs["last"].length < 2) {
+    erreurPrenom = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+    document.getElementById("last_erreur").innerHTML = erreurPrenom;
+  }
+
+  if (inputs["email"].value == "") {
+    erreurPrenom = "Veuillez renseigner votre adresse email.";
+    document.getElementById("email_erreur").innerHTML = erreurPrenom;
+  }
+
+  if (inputs["birthdate"].value == "") {
+    erreurPrenom = "Vous devez entrer votre date de naissance.";
+    document.getElementById("birthdate_erreur").innerHTML = erreurPrenom;
+  }
+
+
+  if (document.querySelector('input[name="location"]:checked')){
+    console.log("valeur radio"+document.querySelector('input[name="location"]:checked').value);
+    } else {
+      erreurVille = "Veuillez saisir une option.";
+      document.getElementById("location_erreur").innerHTML = erreurVille;
+    }
+  
+  if (erreur || erreurVille || erreurPrenom) {
+    e.preventDefault();
+    return false;
+  } else {
+    alert('Formulaire envoyé !');
+  }
+ 
+});
+ 
+function removeWarning() {
+  document.getElementById(this.id + "_erreur").innerHTML = "";
+}
+
+document.getElementById("first").onkeyup = removeWarning;
+document.getElementById("last").onkeyup = removeWarning;
+document.getElementById("email").onkeyup = removeWarning;
+document.getElementById("birthdate").onclick = removeWarning;
